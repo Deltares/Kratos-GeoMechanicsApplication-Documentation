b69c0a3133c0b6ee2f00d94cf96f389eca43ae00
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2026-05-25 12:43:08 +0200

    Generalized the function that makes data series for all stages
    It can now retrieve data for any output item.

--- kratos-docs/docs/examples/crow_case/KratosCROW_7Stage_MohrCoulomb/run_simulation.py
+++ kratos-docs/docs/examples/crow_case/KratosCROW_7Stage_MohrCoulomb/run_simulation.py
@@ -8,8 +8,8 @@ from KratosMultiphysics.project import Project
 import KratosMultiphysics.GeoMechanicsApplication.geo_plot_utilities as plot_utils
 
 
-def _make_bending_moment_data_series_list_for_all_stages(
-    stage_names, nodes_of_sheet_pile_wall
+def _make_data_series_list_for_all_stages(
+    stage_names, nodes_of_sheet_pile_wall, result_item_label
 ):
     """
     Returns a list with one item per stage. Each item is a list of one data
@@ -24,14 +24,14 @@ def _make_bending_moment_data_series_list_for_all_stages(
         json_output_path = Path(f"{name}__output_wall.json")
         with open(json_output_path, "r") as f:
             analysis_results = json.load(f)
-        bending_moments = [
-            analysis_results[f"NODE_{node.Id}"]["BENDING_MOMENT"][0] / 1000.0
+        result_values = [
+            analysis_results[f"NODE_{node.Id}"][result_item_label][0] / 1000.0
             for node in nodes_of_sheet_pile_wall
         ]
 
         # Sort data points by Y coordinate
         sorted_y_coordinates, sorted_bending_moments = zip(
-            *sorted(zip(y_coordinates, bending_moments))
+            *sorted(zip(y_coordinates, result_values))
         )
 
         stage_data_series.append(
@@ -56,14 +56,11 @@ def _plot_bending_moments(nodes_of_sheet_pile_wall):
         "6_Second_excavation",
         "7_Third_excavation",
     ]
-    bending_moment_data_series_per_stage = (
-        _make_bending_moment_data_series_list_for_all_stages(
-            names_of_stages_to_be_plotted, nodes_of_sheet_pile_wall
-        )
-    )
 
     plot_utils.make_sub_plots(
-        bending_moment_data_series_per_stage,
+        _make_data_series_list_for_all_stages(
+            names_of_stages_to_be_plotted, nodes_of_sheet_pile_wall, "BENDING_MOMENT"
+        ),
         Path("bending_moments.svg"),
         titles=names_of_stages_to_be_plotted,
         xlabel="Bending moment [kNm/m]",
