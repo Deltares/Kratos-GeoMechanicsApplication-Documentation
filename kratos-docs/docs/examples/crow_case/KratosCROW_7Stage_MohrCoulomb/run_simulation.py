fee5b578dfd671a2a7c0a6fee63bc684127d7278
Anne van de Graaf <anne.vandegraaf@deltares.nl>
2026-05-25 11:16:38 +0200

    Removed some redundant code

--- kratos-docs/docs/examples/crow_case/KratosCROW_7Stage_MohrCoulomb/run_simulation.py
+++ kratos-docs/docs/examples/crow_case/KratosCROW_7Stage_MohrCoulomb/run_simulation.py
@@ -8,69 +8,6 @@ from KratosMultiphysics.project import Project
 import KratosMultiphysics.GeoMechanicsApplication.geo_plot_utilities as plot_utils
 
 
-def _get_node_ids_of_sheet_pile_wall():
-    return [
-        3968,
-        3982,
-        3996,
-        4010,
-        4026,
-        4036,
-        4051,
-        4066,
-        4075,
-        4090,
-        4102,
-        4120,
-        4135,
-        4148,
-        4161,
-        4172,
-        4188,
-        4204,
-        4218,
-        4231,
-        4241,
-        4256,
-        4271,
-        4280,
-        4297,
-        4308,
-        4322,
-        4338,
-        4352,
-        4361,
-        4375,
-        4390,
-        4403,
-        4415,
-        4430,
-        4444,
-        4457,
-        4470,
-        4483,
-        4497,
-        4509,
-        4522,
-        4536,
-        4547,
-        4560,
-        4571,
-        4582,
-        4595,
-        4604,
-        4614,
-        4624,
-        4633,
-        4639,
-    ]
-
-
-def _get_bending_moment_data_series():
-    node_ids = _get_node_ids_of_sheet_pile_wall()
-    return None
-
-
 def _generate_plots(model):
     print("About to generate plots")
 
@@ -85,21 +22,15 @@ def _generate_plots(model):
     }
 
     sheet_pile_wall = model.GetModelPart("PorousDomain.Sheet_Pile_Wall")
-    print(f"Model part '{sheet_pile_wall.Name}' has {len(sheet_pile_wall.Nodes)} nodes")
 
     y_coordinates = [node.Y for node in sheet_pile_wall.Nodes]
-    for node, y in zip(sheet_pile_wall.Nodes, y_coordinates):
-        print(f"Node {node.Id}: y = {y} (check: {node.Y})")
 
     data_series_per_stage = []
     stages_to_plot = ["wall_installation", "first_excavation", "strut_installation", "second_excavation", "third_excavation"]
-    #stages_to_plot = ["wall_installation"]
     for stage_name in stages_to_plot:
         stage_data_series = []
-        print(f"Retrieving bending moments of stage '{stage_name}'...")
         base_name = stages_info[stage_name]["base_name"]
         json_output_path = Path(f"{base_name}__output_wall.json")
-        # print(f"Reading output from {json_output_path}...")
         with open(json_output_path, "r") as f:
             results = json.load(f)
 
